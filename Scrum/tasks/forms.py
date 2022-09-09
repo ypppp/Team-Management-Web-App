a89085fbe80ee16b55c8ba663ead9e5472f39679
Crookshanks <99803923+Newbie1205@users.noreply.github.com>
2022-09-09 18:36:36 +0800

    Task form bootstrap use and widget update

--- Scrum/tasks/forms.py
+++ Scrum/tasks/forms.py
@@ -3,6 +3,29 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Give your task a title',}
+    ), label='')
+
+    assignee = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Assign a member', }
+    ))
+
+    tag = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Tag a topic', }
+    ))
+
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={'placeholder': 'Add more details to this task',
+               'rows': '5',}
+    ))
+
+    user_story = forms.CharField(widget=forms.Textarea(
+        attrs={'placeholder': 'As a ... I want to ...',
+               'rows': '2'}
+    ))
+
     class Meta:
         model = Task
         fields = [
