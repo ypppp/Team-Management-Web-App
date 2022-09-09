7fd50c75d5877d9a86150a15e3079f4b4cde4a91
nesdai <tche0082@student.monash.edui>
2022-09-09 21:48:55 +0800

    Integration of html for product_backlog.html - COMPLETED Integration of html for task_form.html - COMPLETED (*) Integration of html for task_create_form.html - COMPLETED (*) Integration of html for task_confirm_delete.html - COMPLETED

--- Scrum/tasks/forms.py
+++ Scrum/tasks/forms.py
@@ -3,16 +3,50 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Give your task a title', }
+    ), label='')
+
+    assignee = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Assign a member', }
+    ), required=False)
+
+    tag = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Tag a topic', }
+    ), required=False)
+
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={'placeholder': 'Add more details to this task',
+               'rows': '5', }
+    ))
+
+    user_story = forms.CharField(widget=forms.Textarea(
+        attrs={'placeholder': 'As a ... I want to ...',
+               'rows': '2'}
+    ), required=False)
+
     class Meta:
         model = Task
         fields = [
             'title',
-            'description',
-            'user_story',
             'assignee',
+            'due_date',
             'status',
             'priority',
             'tag',
             'sprint',
-            'due_date'
+            'description',
+            'user_story',
         ]
+
+        widgets = {
+            'due_date': forms.DateTimeInput(
+                format='%Y-%m-%d %H:%M:%S',
+                attrs={'class': 'form-control',
+                       'placeholder': 'Select a date',
+                       'type': 'date'
+                       }),
+
+        }
+
