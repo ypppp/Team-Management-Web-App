97d9742666f901f4c084f3c73bdcd1237fad9587
userr1010 <nath0002@student.monash.edu>
2022-09-28 02:21:29 +0800

    views updated

--- createSprint/createSprintApp/views.py
+++ createSprint/createSprintApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import SprintForm
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
-# Create your views here.
+
+def index(request):
+    form = SprintForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Sprint created successfully!")
+        return HttpResponseRedirect('/')
+    context = {
+        "form": form
+    }
+    return render(request, 'sprint_create_form.html', context)
