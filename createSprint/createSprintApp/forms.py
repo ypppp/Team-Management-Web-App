ec7f882dc1b0515712e0e5008facb7e0689b22bf
userr1010 <nath0002@student.monash.edu>
2022-09-28 02:18:29 +0800

    new form SprintForm created

---
+++ createSprint/createSprintApp/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import Sprint
+
+
+class SprintForm(forms.ModelForm):
+    sprint_name = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Give your sprint a title'}
+    ))
+
+    sprint_goal = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Give your sprint a goal', 'rows': 4}
+    ))
+
+    class Meta:
+        model = Sprint
+        fields = ['sprint_name',
+                  'sprint_goal',
+                  'task',
+                  'assignee',
+                  'duration',
+                  'start_date',
+                  'end_date',
+                  'created_date']
+
+        widgets = {
+            'start_date': forms.DateTimeInput(
+                format='%Y-%m-%d',
+                attrs={'class': 'form-control',
+                       'placeholder': 'Select a date',
+                       'type': 'date'
+                       }),
+            'end_date': forms.DateTimeInput(
+                format='%Y-%m-%d',
+                attrs={'class': 'form-control',
+                       'placeholder': 'Select a date',
+                       'type': 'date'
+                       }),
+
+        }
