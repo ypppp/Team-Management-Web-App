ce47fd5a8650e2d7ef4adf1e6062db314f8132b9
userr1010 <nath0002@student.monash.edu>
2022-09-29 20:23:18 +0800

    removed sprint title field

--- createSprint/createSprintApp/models.py
+++ createSprint/createSprintApp/models.py
@@ -1,6 +1,6 @@
-from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
+from django.utils.datetime_safe import date
 
 
 class Sprint(models.Model):
@@ -18,15 +18,11 @@ class Sprint(models.Model):
         ('T4', 'Task 4')
     ]
 
-    sprint_Name = models.CharField(
-        max_length=100,
-        default="",
-        validators=[RegexValidator('[+-/%]', inverse_match=True)])
-    sprint_Goal = models.CharField(max_length=250, default="")
+    sprint_goal = models.CharField(max_length=250, default="")
     task = models.CharField(blank=True, choices=TASKS, max_length=20)
-    assignee = models.CharField(max_length=100)
+    assignee = models.CharField(max_length=100, default="")
     duration = models.CharField(blank=True, choices=DURATION_LEVELS, max_length=10)
-    start_Date = models.DateTimeField(blank=True, null=True)
-    end_Date = models.DateTimeField(blank=True, null=True)
-    created_Date = models.DateTimeField(default=timezone.now, auto_now_add=False)
-    is_Sprint_Complete = models.BooleanField(default=False)
+    start_date = models.DateField(default=timezone.now())
+    end_date = models.DateField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now, auto_now_add=False)
+
