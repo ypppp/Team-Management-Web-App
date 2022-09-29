a7b5e6cb7895cb684241ddd65ecf7917ed711489
userr1010 <nath0002@student.monash.edu>
2022-09-29 13:52:47 +0800

    field names updated

--- createSprint/createSprintApp/forms.py
+++ createSprint/createSprintApp/forms.py
@@ -3,34 +3,34 @@ from .models import Sprint
 
 
 class SprintForm(forms.ModelForm):
-    sprint_name = forms.CharField(widget=forms.TextInput(
+    sprint_Name = forms.CharField(widget=forms.TextInput(
         attrs={'placeholder': 'Give your sprint a title'}
     ))
 
-    sprint_goal = forms.CharField(widget=forms.TextInput(
+    sprint_Goal = forms.CharField(widget=forms.TextInput(
         attrs={'placeholder': 'Give your sprint a goal', 'rows': 4}
     ))
 
     class Meta:
         model = Sprint
-        fields = ['sprint_name',
-                  'sprint_goal',
+        fields = ['sprint_Name',
+                  'sprint_Goal',
                   'task',
                   'assignee',
                   'duration',
-                  'start_date',
-                  'end_date',
-                  'created_date',
-                  'is_sprint_complete']
+                  'start_Date',
+                  'end_Date',
+                  'created_Date',
+                  'is_Sprint_Complete']
 
         widgets = {
-            'start_date': forms.DateTimeInput(
+            'start_Date': forms.DateTimeInput(
                 format='%d-%m-%Y',
                 attrs={'class': 'form-control',
                        'placeholder': 'Select a date',
                        'type': 'date'
                        }),
-            'end_date': forms.DateTimeInput(
+            'end_Date': forms.DateTimeInput(
                 format='%d-%m-%Y',
                 attrs={'class': 'form-control',
                        'placeholder': 'Select a date',
