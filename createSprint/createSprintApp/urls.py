f1b58766e1071e27f6c124a338995ba8820aaa33
userr1010 <nath0002@student.monash.edu>
2022-09-29 20:32:49 +0800

    urls updated

--- createSprint/createSprintApp/urls.py
+++ createSprint/createSprintApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SprintCreateView
 
 urlpatterns = [
-    path('', SprintCreateView.as_view(), name='sprint_create_form'),
+    path('', SprintCreateView.as_view(), name='create_sprint_form'),
 ]
