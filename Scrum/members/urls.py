d3013f963606609558263678782c60e4e38b9c78
userr1010 <nath0002@student.monash.edu>
2022-10-07 12:13:47 +0800

    urls added

--- Scrum/members/urls.py
+++ Scrum/members/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import MemberListView
-
+from .views import MemberListView, MemberCreateView
 
 urlpatterns = [
     path('', MemberListView.as_view(), name='member-list'),
+    path('member/new/', MemberCreateView.as_view(), name='member-create')
 ]
