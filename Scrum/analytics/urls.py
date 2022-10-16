764a99ca874250e20b39738738658f7f7699a17b
Crookshanks <99803923+Newbie1205@users.noreply.github.com>
2022-10-16 15:58:32 +0800

    analytics problems to fix: entryform: - min max date -> sprint start end - queryset -> tasks of ongoing sprint
    views.py & analytics.html:
    - data access hooowww wtfff dict can be accessed
    - fuck custom tags
    -

--- Scrum/analytics/urls.py
+++ Scrum/analytics/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import DailyTeamAnalytics, RecentEntryView, AddEntryView
 
 urlpatterns = [
-    path('daily/', DailyTeamAnalytics.as_view(), name='team-stat'),
+    path('', DailyTeamAnalytics.as_view(), name='team-anal'),
     path('entries/', RecentEntryView.as_view(), name='entry-list'),
     path('entry/new/', AddEntryView.as_view(), name='entry-create'),
 ]
