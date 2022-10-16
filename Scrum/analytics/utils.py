3a80cf11027206595d069d49712f7d2528c91a03
Crookshanks <99803923+Newbie1205@users.noreply.github.com>
2022-10-16 21:51:45 +0800

    Analytics
    EntryForm:
    - 24 validations completed
    - queryset problem solved
    
    views.py:
    - context data fixed
    - entry create complete
    
    templates:
    - entry_form.html complete
    - add entry buttons created
    - analytics.html displays data
    
    utils.py:
    - creation
    - helper methods
    - sprint daily data & dayrange complete

---
+++ Scrum/analytics/utils.py
@@ -0,0 +1,59 @@
+from datetime import timedelta
+
+from django.db.models import Sum
+from django.utils.datetime_safe import date
+
+from analytics.models import Entry
+from sprints.models import Sprint
+
+
+def get_sum(sprint: Sprint) -> float:
+    """
+    Computes the sum of work hours for a sprint
+
+    """
+    total = 0
+    return total
+
+
+def get_sprint_data(sprint: Sprint) -> tuple[list, list]:
+    """
+    Computes and returns daily work hours data for a sprint
+
+    """
+    # get sprint date range
+    start_date = sprint.start_date
+    end_date = sprint.end_date
+
+    # initialise values
+    day_range = get_day_range(start_date, end_date)
+    hours = []
+
+    # operation
+    for day in day_range:
+        entries = Entry.objects.filter(date=day)
+        duration = 0
+
+        if entries.count() > 0:
+            agg = entries.aggregate(total=Sum('duration'))
+            duration = agg['total']
+            duration = int(duration.days * 24)
+
+        hours.append(duration)
+
+    return day_range, hours
+
+
+def get_day_range(start: date, end: date, step_size=1) -> list[date]:
+    """
+    Returns the dates within the specified range inclusive
+
+    """
+    delta = end - start
+    day_range = []
+
+    for day in range(delta.days + step_size):
+        td = start + timedelta(days=day)
+        day_range.append(td)
+
+    return day_range
